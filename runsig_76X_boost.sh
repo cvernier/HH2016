3db812c816158abb95a8eb87b26c8a7cba9ce80b
Caterina <caterina.vernieri@cern.ch>
2016-07-06 12:34:32 -0500

    first commit

---
+++ runsig_76X_boost.sh
@@ -0,0 +1,127 @@
+#/bin/sh
+
+#BG 1200
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135235/0000/  --outName=BG_1000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt  --syst=None&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135235/0000/  --outName=BG_1000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135235/0000/  --outName=BG_1000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135235/0000/  --outName=BG_1000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135235/0000/  --outName=BG_1000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135235/0000/  --outName=BG_1000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135235/0000/  --outName=BG_1000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Down&
+
+
+#python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-1200_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-1200_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_141815/0000/  --outName=BG_1200_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1200.txt &
+
+#BG 1400
+
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135329/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt  --syst=None&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135329/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135329/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135329/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135329/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135329/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135329/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Down&
+
+
+
+
+
+
+
+#BG 1600
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135401/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt  --syst=None&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135401/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135401/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135401/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135401/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135401/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135401/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Down&
+
+
+#BG 1800
+#python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-1800_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-1800_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_142027/0000/  --outName=BG_1800_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1200.txt &
+
+#BG2000
+
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135507/0000/ --outName=BG_2000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt  --syst=None&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135507/0000/ --outName=BG_2000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135507/0000/ --outName=BG_2000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135507/0000/ --outName=BG_2000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135507/0000/ --outName=BG_2000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135507/0000/ --outName=BG_2000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135507/0000/ --outName=BG_2000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Down&
+
+
+#BG 2500
+#python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_142200/0000/  --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG2500.txt &
+
+
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135540/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt --syst=None &
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135540/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135540/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135540/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135540/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135540/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21c_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160504_135540/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Down&
+
+
+
+
+
+
+#BG 3000
+#python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160316_151510/0000/  --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG3000.txt &
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21b_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160426_150414/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt  --syst=None&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21b_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160426_150414/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21b_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160426_150414/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21b_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160426_150414/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21b_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160426_150414/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=MJEC_Down&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21b_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160426_150414/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Up&
+
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21c/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21b_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160426_150414/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1000.txt   --syst=FJER_Down&
+
+
+
+#BG 4000
+#python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-4000_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-4000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_142613/0000/  --outName=BG_4000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG4000.txt &
+

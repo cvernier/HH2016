7fe02723c6615cbe792fa667d73c125d1a00bea1
Caterina <caterina.vernieri@cern.ch>
2016-07-06 12:36:34 -0500

    first commit

---
+++ hh13TeVconfig/run_prepare.sh
@@ -0,0 +1,18 @@
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S QCD500
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S QCD700
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S QCD1000
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S QCD1500
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S WQQ
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S TT
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S WZ
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S ZZ
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S WW
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S JetHT
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S QCD2000
+./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S JetHT_top
+#./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S TTHT600
+#./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S TTHT800
+#./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S TTHT1200
+#./prepare_environment_with_config.py -C hh13TeVconfig/paths -C hh13TeVconfig/samples_nosplit.cfg -S TTHT2500
+
+
